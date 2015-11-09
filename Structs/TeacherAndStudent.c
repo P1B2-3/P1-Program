2c15de683e4642c574bc179fa579c49d134a5681
GeneralDenmark <foo-fighters@live.dk>
2015-11-09 08:12:18 +0100

    Create TeacherAndStudent.c

---
+++ Structs/TeacherAndStudent.c
@@ -0,0 +1,26 @@
+
+struct Data{
+    int unikID[10000];
+    char Fornavn[5000];
+    char Efternavn[5000];
+    int Mm[5000];
+    int Sm[5000];
+    int Md[5000];
+    int Sd[5000];
+    int Mf[5000];
+    int Sf[5000];
+    int Me[5000];
+    int Se[5000];
+    int Mk[5000];
+    int Sk[5000];
+    int Mt[5000];
+    int Mp[5000];
+    int Mi[5000];
+    int Mb[5000];
+    int Mki[5000];
+    int Ms[5000];
+    
+
+};
+
+int main(){
