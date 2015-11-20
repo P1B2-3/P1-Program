565410505579cd879c90fbbc1b2e291fa3b3e92f
joakim347 <joakim347@gmail.com>
2015-11-20 10:46:49 +0100

    Update TeacherAndStudent.c

--- Structs/TeacherAndStudent.c
+++ Structs/TeacherAndStudent.c
@@ -1,23 +1,23 @@
 
 struct Data{
-    int unikID[10000];
-    int Mm[5000];
-    int Sm[5000];
-    int Md[5000];
-    int Sd[5000];
-    int Mf[5000];
-    int Sf[5000];
-    int Me[5000];
-    int Se[5000];
-    int Mk[5000];
-    int Sk[5000];
-    int Mt[5000];
-    int Mp[5000];
-    int Mi[5000];
-    int Mb[5000];
-    int Mki[5000];
-    int Ms[5000];
-    
+    int unikID;
+    int Mm; /*Mundlig matmatik */ 
+    int Sm; /*Skriftlig matmatik */ 
+    int Md; /*Mundlig dansk */ 
+    int Sd; /*Skriftlig dansk */ 
+    int Mf; /*Mundlig fysik */ 
+    int Sf; /*Skriftlig fysik */ 
+    int Me; /*Mundlig engelsk */ 
+    int Se; /*Skriftlig engelsk */ 
+    int Mk; /*Mundlig kemi */ 
+    int Sk; /*Skriftlig kemi */ 
+    int Mt; /*Mundlig teknologi */ 
+    int Mp; /*Mundlig programmering */ 
+    int Mi; /*Mundlig indehistorie */ 
+    int Mb; /*Mundlig biologi */ 
+    int Mki;/*Mundlig kom it */ 
+    int Ms; /*Mundlig samfundsfag */
+    int Mth; /*Mundlig teknologi historie */
 
 };
 
