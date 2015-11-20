cd5b8df4dc38a14503280809fada748cb9bd4417
GeneralDenmark <foo-fighters@live.dk>
2015-11-20 14:46:38 +0100

    Create Get_exames_student.c

---
+++ Sorting/Get_exames_student.c
@@ -0,0 +1,8 @@
+/*
+Denne funktion, skal kunne fortælle i et nyt array hvilke studenter der skal op til hvilke eksamener.
+Dette skal den gøre ved at køre qsort funktionen ->INSERT NAME HERE<-
+
+Programmet er skrevet af: Asger Geel Weirsøe
+20/11-2015
+V. PRE-ALPHA
+*/
