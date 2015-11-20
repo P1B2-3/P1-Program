b90fa41ac74d6693ce6a2f3fc68a23c615c46f37
GeneralDenmark <foo-fighters@live.dk>
2015-11-20 13:40:45 +0100

    Update sort_for_examns.c

--- Sorting/sort_for_examns.c
+++ Sorting/sort_for_examns.c
@@ -1,36 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void sort_kort_mat_mundtlig(data student_t[], int last){
+void Sort_kort_mat_mundtlig(data student_t[], int last){
   
   
 }
-void sort_kort_mat_mundtlig(data student_t[], int last){
+void Sort_kort_mat_skriftlig(data student_t[], int last){
   
   
 }
-void sort_kort_mat_mundtlig(data student_t[], int last){
+void Sort_kort_da_mundtlig(data student_t[], int last){
   
   
 }
-void sort_kort_mat_mundtlig(data student_t[], int last){
+void Sort_kort_da_skriftlig(data student_t[], int last){
   
   
 }
-void sort_kort_mat_mundtlig(data student_t[], int last){
+void Sort_kort_fys_mundtlig(data student_t[], int last){
   
   
 }
-void sort_kort_mat_mundtlig(data student_t[], int last){
+void Sort_kort_fys_skriftlig(data student_t[], int last){
   
   
-}void sort_kort_mat_mundtlig(data student_t[], int last){
+}
+void Sort_kort_eng_mundtlig(data student_t[], int last){
   
   
-}void sort_kort_mat_mundtlig(data student_t[], int last){
+}
+void Sort_kort_eng_skriftlig(data student_t[], int last){
   
   
-}void sort_kort_mat_mundtlig(data student_t[], int last){
+}
+void Sort_kort_kem_mundtlig(data student_t[], int last){
+  
+  
+}
+void Sort_kort_kem_skriftlig(data student_t[], int last){
+  
+  
+}
+void Sort_kort_tek_mundtlig(data student_t[], int last){
+  
+  
+}
+void Sort_kort_pro_mundlig(data student_t[], int last){
+  
+  
+}
+void Sort_kort_ide_mundtlig(data student_t[], int last){
+  
+  
+}
+void Sort_kort_bio_mundtlig(data student_t[], int last){
+  
+  
+}
+void Sort_kort_komit_mundtlig(data student_t[], int last){
+  
+  
+}
+void Sort_kort_samf_mundtlig(data student_t[], int last){
   
   
 }
