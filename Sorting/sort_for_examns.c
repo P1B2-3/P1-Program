a0bb21cdde91f00ab5b546cfcf4a3477015b049a
GeneralDenmark <foo-fighters@live.dk>
2015-11-20 13:35:56 +0100

    Update sort_for_examns.c

--- Sorting/sort_for_examns.c
+++ Sorting/sort_for_examns.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+void sort_kort_mat_mundtlig(data student_t[], int last){
+  
+  
+}
+void sort_kort_mat_mundtlig(data student_t[], int last){
+  
+  
+}
+void sort_kort_mat_mundtlig(data student_t[], int last){
+  
+  
+}
+void sort_kort_mat_mundtlig(data student_t[], int last){
+  
+  
+}
+void sort_kort_mat_mundtlig(data student_t[], int last){
+  
+  
+}
+void sort_kort_mat_mundtlig(data student_t[], int last){
+  
+  
+}
