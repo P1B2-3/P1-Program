77f675c903b6dfa324a9d1cbfd9675d627fc23c3
GeneralDenmark <foo-fighters@live.dk>
2015-11-20 14:38:51 +0100

    Update sort_for_examns.c

--- Sorting/sort_for_examns.c
+++ Sorting/sort_for_examns.c
@@ -1,67 +1,280 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void Sort_kort_mat_mundtlig(data student_t[], int last){
+void Sort_data_student(data student_t[], int last,char * examen) {
+  if(examen == "matmund") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_mat_mundtlig);
+  }else if(examen == "matskrif") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_mat_skriftlig);
+  }else if(examen == "damund") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_da_mundtlig);
+  }else if(examen == "daskrif") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_da_skriftlig);
+  }else if(examen == "fysmund") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_fys_mundtlig);
+  }else if(examen == "fysskrif") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_fys_skriftlig);
+  }else if(examen == "engmund") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_eng_mundtlig);
+  }else if(examen == "engskrif") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_eng_skriftlig);
+  }else if(examen == "kemmund") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_kem_mundtlig);
+  }else if(examen == "kemskrif") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_kem_skriftlig);
+  }else if(examen == "tek") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_tek);
+  }else if(examen == "prog") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_prog);
+  }else if(examen == "ideh") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_ideh);
+  }else if(examen == "bio") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_bio);
+  }else if(examen == "komit") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_komit);
+  }else if(examen == "samf") {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_samf);
+  }else {
+    qsort(stydebt_t,last+1,sizeof(data),qsort_tekhis);
+  }
   
   
 }
-void Sort_kort_mat_skriftlig(data student_t[], int last){
-  
-  
+int qsort_mat_mundtlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+    if(a->Mm && b->Mm) {
+      if(a->unikID < b->unikID) {
+        return 1;
+      }
+    }
+    else if(a->Mm && !(b->Mm) {
+      return 1;
+    }
+  return -1;
 }
-void Sort_kort_da_mundtlig(data student_t[], int last){
-  
-  
+int qsort_mat_skriftlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+    if(a->Sm && b->Sm) {
+      if(a->unikID < b->unikID) {
+        return 1;
+      }
+    }
+    else if(a->Sm && !(b->Sm)) {
+      return 1;
+    }
+  return -1;
 }
-void Sort_kort_da_skriftlig(data student_t[], int last){
-  
-  
+int qsort_da_mundtlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Md && b->Md) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Md && !(b->Md)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_fys_mundtlig(data student_t[], int last){
-  
-  
+int qsort_da_skriftlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Sd && b->Sd) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Sd && !(b->Sd)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_fys_skriftlig(data student_t[], int last){
-  
-  
+int qsort_fys_mundtlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Mf && b->Mf) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Mf && !(b->unikID)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_eng_mundtlig(data student_t[], int last){
-  
-  
+int qsort_fys_skriftlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Sf && b->Sf) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Sf && !(b->Sf)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_eng_skriftlig(data student_t[], int last){
-  
-  
+int qsort_eng_mundtlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Me && b->Me) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Me && !(b->Me)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_kem_mundtlig(data student_t[], int last){
-  
-  
+int qsort_eng_skriftlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Se && b->Se) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Se && !(b->Se)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_kem_skriftlig(data student_t[], int last){
-  
-  
+int qsort_kem_mundtlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Mk && b->Mk) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->unikID && !(b->unikID)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_tek_mundtlig(data student_t[], int last){
-  
-  
+int qsort_kem_skriftlig(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Sk && b->Sk) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Sk && !(b->Sk)) {
+    return 1;
+  }
+  retuen -1;
 }
-void Sort_kort_pro_mundlig(data student_t[], int last){
-  
-  
+int qsort_tek(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Mt && b->Mt) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Mt && !(b->Mt)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_ide_mundtlig(data student_t[], int last){
-  
-  
+int qsort_prog(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Mp && b->Mp) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Mp && !(b->Mp)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_bio_mundtlig(data student_t[], int last){
-  
-  
+int qsort_ideh(const void* p1,const void* p2) {
+ data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Mi && b->Mi) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Mi && !(b->Mi)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_komit_mundtlig(data student_t[], int last){
-  
-  
+int qsort_bio(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Mb && b->Mb) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Mb && !(b->Mb)) {
+    return 1;
+  }
+  return -1;
 }
-void Sort_kort_samf_mundtlig(data student_t[], int last){
-  
-  
+int qsort_komit(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Mki && b->Mki) {
+    if(a->unikID < b->unikID) {
+      return 1;
+    }
+  }
+  else if(a->Mki && !(b->Mki)) {
+    return 1;
+  }
+  return -1;
+}
+int qsort_samf(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Ms && b->Ms) {
+    if(a->unikID < b->unikId) {
+      return 1;
+    }
+  }
+  else if(a->Ms && !(b->Ms)) {
+    return 1;
+  }
+  return -1;
+}
+int qsort_tekhis(const void* p1,const void* p2) {
+  data *a,*b;
+    a = (data*)p1;
+    b = (data*)p2;
+  if(a->Mth && b->Mth) {
+    if(a->unikID < b->unikId) {
+      return 1;
+    }
+  }
+  else if(a->Mth && !(b->Mth)) {
+    return 1;
+  }
+  return -1;
 }
