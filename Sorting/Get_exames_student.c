5c2c907bde870cb65fb26d03e8dc0ffdb73d0e13
GeneralDenmark <foo-fighters@live.dk>
2015-11-21 17:58:43 +0100

    Update Get_exames_student.c

--- Sorting/Get_exames_student.c
+++ Sorting/Get_exames_student.c
@@ -3,6 +3,18 @@ Denne funktion, skal kunne fortælle i et nyt array hvilke studenter der skal op
 Dette skal den gøre ved at køre qsort funktionen ->INSERT NAME HERE<-
 
 Programmet er skrevet af: Asger Geel Weirsøe
-20/11-2015
-V. PRE-ALPHA
+21/11-2015
+V. 1.00
 */
+int Get_examens_student(Data_elever_t Data_elever_t[], int last,char * examen) {
+  Sort_for_examns(Data_elever_t[], last,examen)
+  
+  
+  
+}
+
+Data_elever_t Create_new_database_for_students(){
+  for()
+  Data_elever_t 
+  
+}
