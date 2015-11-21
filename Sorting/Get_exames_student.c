63d410817cde04550a8239e548c18cd219f87a2a
GeneralDenmark <foo-fighters@live.dk>
2015-11-21 18:23:11 +0100

    Update Get_exames_student.c

--- Sorting/Get_exames_student.c
+++ Sorting/Get_exames_student.c
@@ -6,15 +6,19 @@ Programmet er skrevet af: Asger Geel Weirsøe
 21/11-2015
 V. 1.00
 */
+char * exmn_var;
 int Get_examens_student(Data_elever_t Data_elever_t[], int last,char * examen) {
   Sort_for_examns(Data_elever_t[], last,examen)
-  
+  exmn_var = examen;
   
   
 }
 
-Data_elever_t Create_new_database_for_students(){
-  for()
-  Data_elever_t 
-  
+Data_after_sorting_t Create_new_database_for_students(Data_elever_t[],int unikID, char * student_first_name, char * student_last_name){
+  int i=0;
+  for(;Data_elever_t->examen == 1;i++)
+  Data_after_sorting_t results;
+  results.unikID = unikID;
+  results.for_navn = student_first_name;
+  results.efter_navn = student_last_name;
 }
