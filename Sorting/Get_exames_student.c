528b990edb812039a4a489cdb79d7183f13e6efb
GeneralDenmark <foo-fighters@live.dk>
2015-11-24 10:32:54 +0100

    Update Get_exames_student.c

--- Sorting/Get_exames_student.c
+++ Sorting/Get_exames_student.c
@@ -4,17 +4,19 @@ Dette skal den gøre ved at køre qsort funktionen ->INSERT NAME HERE<-
 
 Programmet er skrevet af: Asger Geel Weirsøe
 24/11-2015
-V. 1.02
+V. 1.04
 */
-char * exmn_var;
+char * glob_examen;
 int Get_examens_student(Data_elever_t Data_elever_t[], int last,char * examen, Data_after_sorting_t Data_elever_after_sorting_t[];) {
-    Sort_for_examns(Data_elever_t[], Data_elever_after_sorting_t[])
-    exmn_var = examen;
+    glob_examen = examen;
+    Sort_for_examns(Data_elever_t[],last,examen)
+    Create_new_database(Data_elever_t[],Data_elever_after_sorting_t[]);
+
 }
 
 void Create_new_database(Data_after_sorting_t Data_elever_t[],Data_after_sorting_t Data_elever_after_sorting_t[]){
     int i=0;
-    for(;Data_elever_t->examen == 1;i++) {
+    for(;Data_elever_t[i]->glob_examen == 1;i++) {
         Data_elever_after_sorting_t[i] =
         Create_new_students(Data_elever_t.unikID,Data_elever_t.elev_for_navn,Data_elever_t.elev_efter_navn);
   }
