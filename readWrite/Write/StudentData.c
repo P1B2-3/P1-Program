7942bdfb0ef5ab1c346d3a745c5702d0f1acb4ca
GeneralDenmark <foo-fighters@live.dk>
2015-11-24 10:45:52 +0100

    Update StudentData.c

--- readWrite/Write/StudentData.c
+++ readWrite/Write/StudentData.c
@@ -7,15 +7,6 @@ Date:           9/11
 Version:        1.04
 */
 
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdio.h>
-   
-
-
 int StudentData(int *studentData,char studentFirstName,char studentLastName,int numOfStudents){
     #if defined(_WIN32)
     _mkdir("./data");
