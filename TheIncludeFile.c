a6b904ff9424fa5c9e636e648a71b7bba354b56a
GeneralDenmark <foo-fighters@live.dk>
2015-11-24 10:47:18 +0100

    Update TheIncludeFile.c

--- TheIncludeFile.c
+++ TheIncludeFile.c
@@ -5,6 +5,12 @@ You know.. The new school-(reform?) says, that we should be better to include ot
 So, now.. Because of this, we've made a file, only to include.
 We love you special kids! <3
 */
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
 #include "Structs/DATA_BASE.h" /* struct data */
 #include "readWrite/Write/DaysWhereTeacherIsAbcent.c"
 #include "readWrite/Write/StudentData.c"
