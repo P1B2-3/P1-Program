c627035a467a5b83063770de2cbd46204545a0b4
GeneralDenmark <foo-fighters@live.dk>
2015-11-24 10:45:36 +0100

    Update DaysWhereTeacherIsAbcent.c

--- readWrite/Write/DaysWhereTeacherIsAbcent.c
+++ readWrite/Write/DaysWhereTeacherIsAbcent.c
@@ -7,16 +7,6 @@ Date:           9/11
 Version:        1.02
 */
 
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <stdio.h>
-
- 
-
-
 int DaysWhereTeacherIsAbcent(int *teacherDaysData, char teacherFirstName, char teacherLastName, int numOfTeachers){
      #if defined(_WIN32)
     _mkdir("./data");
