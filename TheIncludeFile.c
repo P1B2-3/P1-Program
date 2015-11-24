be5e0d5beb6628479f69f801855a50db5a8d1e14
GeneralDenmark <foo-fighters@live.dk>
2015-11-24 10:41:10 +0100

    Update TheIncludeFile.c

--- TheIncludeFile.c
+++ TheIncludeFile.c
@@ -5,3 +5,10 @@ You know.. The new school-(reform?) says, that we should be better to include ot
 So, now.. Because of this, we've made a file, only to include.
 We love you special kids! <3
 */
+#include "Structs/DATA_BASE.h" /* struct data */
+#include "readWrite/Write/DaysWhereTeacherIsAbcent.c"
+#include "readWrite/Write/
+#include "readWrite/Write/
+#include "readWrite/Write/
+#include "readWrite/Write/
+#include "readWrite/Write/
