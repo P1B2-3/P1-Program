327f986ccbe4bb7e529485ba58871aa4f239aba9
GeneralDenmark <foo-fighters@live.dk>
2015-11-24 10:39:20 +0100

    Create TheIncludeFile.c

---
+++ TheIncludeFile.c
@@ -0,0 +1,7 @@
+/*
+File just to include, includers..
+
+You know.. The new school-(reform?) says, that we should be better to include others, rather than exclude...
+So, now.. Because of this, we've made a file, only to include.
+We love you special kids! <3
+*/
