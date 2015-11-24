6ed40cdb53dc7a5501870fb5df4b99cbd8b6b774
GeneralDenmark <foo-fighters@live.dk>
2015-11-24 10:23:00 +0100

    Update Get_exames_student.c

--- Sorting/Get_exames_student.c
+++ Sorting/Get_exames_student.c
@@ -7,18 +7,27 @@ Programmet er skrevet af: Asger Geel Weirsøe
 V. 1.00
 */
 char * exmn_var;
-int Get_examens_student(Data_elever_t Data_elever_t[], int last,char * examen) {
-  Sort_for_examns(Data_elever_t[], last,examen)
-  exmn_var = examen;
+int Get_examens_student(Data_elever_t Data_elever_t[], int last,char * examen, Data_after_sorting_t Data_elever_after_sorting_t[];) {
+    Sort_for_examns(Data_elever_t[], Data_elever_after_sorting_t[])
+    exmn_var = examen;
   
   
 }
 
-Data_after_sorting_t Create_new_database_for_students(Data_elever_t[],int unikID, char * student_first_name, char * student_last_name){
-  int i=0;
-  for(;Data_elever_t->examen == 1;i++)
-  Data_after_sorting_t results;
-  results.unikID = unikID;
-  results.for_navn = student_first_name;
-  results.efter_navn = student_last_name;
+void Create_new_database(Data_after_sorting_t Data_elever_t[],Data_after_sorting_t Data_elever_after_sorting_t[]){
+    int i=0;
+    for(;Data_elever_t->examen == 1;i++) {
+        Data_elever_after_sorting_t[i] =
+        Create_new_students(Data_elever_t.unikID,Data_elever_t.elev_for_navn,Data_elever_t.elev_efter_navn);
+  }
+}
+
+Data_after_sorting_t Create_new_students(int unikID, char * student_first_name, char * student_last_name) {
+    Data_after_sorting_t results;
+    
+    results.unikID = unikID;
+    results.for_navn = student_first_name;
+    results.efter_navn = student_last_name;
+    
+    return results;
 }
