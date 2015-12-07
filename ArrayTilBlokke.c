c58e3e60baffdffa8e494e3e9420c12a20d4f26c
PalleThillemann <palle95@gmail.com>
2015-12-07 13:46:47 +0100

    Create ArrayTilBlokke.c

---
+++ ArrayTilBlokke.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HOURS 8
+#define DAYS 5
+#define WEEKS 4
+#define MONTHS 1
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for(i = 1; i <= MONTHS; i++) {
+		for(j = 1; j <= WEEKS; j++) {
+			for(k = 1; k <= DAYS; k++) {
+				for(l = 1; l <= HOURS; l++) {
+
+				}
+			}
+		}
+	}
+}
