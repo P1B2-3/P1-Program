2aabb21ea5083f628dcff0f14b08c8da4cdf5b19
GeneralDenmark <foo-fighters@live.dk>
2015-12-14 09:31:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,27 +2,31 @@
 #include "TheIncludeFile.h" /*File to include!*/
 
 
-int main(void){
-	/* Variabels */
-	int studentAmount = getConfig(), /* data fra config */
-		teacherAmount = getConfig(),
-		eksamenAmount = getConfig(),
-		roomAmount    = getConfig();
+int main (void){
+    int studentAmount = getConfig(), /* data fra config */
+        teacherAmount = getConfig(),
+        eksamenAmount = getConfig(),
+        roomAmount    = getConfig();
 
-	/* Array af structs */
-	Data_students_t 		StudentData[studentAmount];
-	Data_Teachers_t 		TeacherData[teacherAmount];
-	Data_eksamen_t 			EksamenData[eksamenAmount]:
-	Data_room_t 			RoomData[roomAmount];
-	Data_after_sorting_t 		DataSort[studentAmount];
-	 
+    /* Array af structs */
+    Data_students_t         StudentData[studentAmount];
+    Data_Teachers_t         TeacherData[teacherAmount];
+    Data_eksamen_t          EksamenData[eksamenAmount]:
+    Data_room_t             RoomData[roomAmount];
+    Data_after_sorting_t        DataSort[studentAmount];
 
-	/* funktion der indsætter elev data */
-	/* funktion der indsætter eksamner for eleverne fra klasserne */
+    Exam_block_t *****schema_data = Make_schema();
 
+    int i,j;
+    for (i = 0; i < ROOM_SIZE; i++)
+    {
+        for (j = 0; j < DAY_SIZE; j++)
+        {
+            printf("year %i, day %i, room %i\n",schema_data[0][0][j][i][0].year, j, i);
+        }
+    }
 
-
-	return (0);
+    return 0;
 }
 
 /* items */
