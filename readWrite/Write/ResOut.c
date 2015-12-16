bcb5bbf4faf642b3432ce21eb7228be68758f963
GeneralDenmark <foo-fighters@live.dk>
2015-12-16 21:49:23 +0100

    Funktion der kan skrive det bedste skema ud til en fil.

---
+++ readWrite/Write/ResOut.c
@@ -0,0 +1,67 @@
+
+void ResultOut(Exam_block_t *****schema_data) {
+    
+    char c;
+    FILE *fptr;
+
+    if(fptr = fopen("best_schema.txt" "r") == NULL) {
+        printf("Schema not existing creating new.\n");
+        write(schema_data,fptr);
+    }
+    else {
+        printf("!Schema existing!\n Do you want to create new? [Y]es or [N]o: ");
+        do{
+            scanf("%c" &c);
+            printf("\n");
+            if(c == 'N' || c == 'n'){
+                printf("exitting.\n");
+                assert(0);
+            }
+            else if(c == 'Y' || c == 'y') {
+                write(schema_data,fptr);
+                break;
+            }
+            else {
+                printf("Please indput either Y or N for [Y]es or [N]o :");
+            }
+        }while(1)
+
+    }
+    
+    fclose(fptr);
+}
+void write(Exam_block_t *****schema_data, FILE *fptr) {
+    int i,k,j,m,n;
+
+    fprintf(fptr,"Best solution.\n");
+    
+    for(k = 0; k < WEEK_SIZE; k++)
+    {
+        fprintf(fptr,"For week: %i\n",k);
+        
+        for(j = 0; j < DAY_SIZE; j++)
+        {
+            fprintf(fptr,"For day: %i\n",j);
+            
+            for(m = 0; m < ROOM_SIZE; m++)
+            {
+                fprintf(fptr,"For room: %i\n",m);
+                
+                for(i = 0; i < EXAM_SIZE; i++)
+                {
+                    fprintf(fptr, "For exam: %i",i);
+                    fprintf(fptr, "Exam name: %s",schema_data[0][k][j][m][i].subjects.subjects);
+                    fprintf(fptr, "Class: %c.%i\n",schema_data[0][k][j][m][i].classname,schema_data[0][k][j][m][i].year);
+                    fprintf(fptr, "Student id: %i\n",schema_data[0][k][j][m][i].students.unikID);
+                    fprintf(fptr, "Student name: %s %s\n\n",schema_data[0][k][j][m][i].students.name,schema_data[0][k][j][m][i].students.surname);
+                    fprintf(fptr, "Teacher id: %i\n",schema_data[0][k][j][m][i].subjects.teacher.ID);
+                    fprintf(fptr, "Teacher name: %s %s\n\n",schema_data[0][k][j][m][i].subjects.teacher.name,schema_data[0][k][j][m][i].subjects.teacher.surname);
+                    fprintf(fptr, "Censor id: %i \n",schema_data[0][k][j][m][i].subjects.censor.ID);
+                    fprintf(fptr, "Censor name: %s %s\n",schema_data[0][k][j][m][i].subjects.censor.name,schema_data[0][k][j][m][i].subjects.censor.surname);
+
+
+                }
+            }
+        }
+    } 
+}
