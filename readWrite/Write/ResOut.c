f6e663e9f5700b32bc578b45ddb7f716b51e4447
GeneralDenmark <foo-fighters@live.dk>
2015-12-17 17:02:48 +0100

    Getting the schema with highest fitness.

--- readWrite/Write/ResOut.c
+++ readWrite/Write/ResOut.c
@@ -1,14 +1,33 @@
 
-void WriteToFile(Exam_block_t *****schema_data, FILE *fptr);
+void WriteToFile(Exam_block_t *****schema_data, int schemaNr, FILE *fptr);
 void ResultOut(Exam_block_t *****schema_data) {
+    int i,b,c,d,k,totalFitness = 0,tempFitness = 0,schemaNr;
     char temp_char;
     FILE *fptr;
+    
+    for(i = 0; i < SCHEMA_SIZE; i++) {
 
+        for(b = 0; b < WEEK_SIZE; b++) {
+            for(c = 0; c < DAY_SIZE; c++) {
+                for(d = 0; d < ROOM_SIZE; d++) {
+                    for(k = 0; k < EXAM_SIZE; k++) {
+                        tempFitness += schema_data[i][b][c][d][k].fitness;
+                    }
+                }
+            }
+        }
+
+        if(tempFitness > totalFitness) {
+            totalFitness = tempFitness;
+            schemaNr = i;
+        }
+    }
       
     if ((fptr=fopen("best_schema.txt","r"))==NULL){
         printf("Schema not existing creating new.\n");
-        WriteToFile(schema_data,fptr);
+        WriteToFile(schema_data,schemaNr,fptr);
     }
+    
     else {
 
         printf("!Schema existing!\n Do you want to create new? [Y]es or [N]o: ");
@@ -20,7 +39,7 @@ void ResultOut(Exam_block_t *****schema_data) {
                 assert(0);
             }
             else if(temp_char == 'Y' || temp_char == 'y') {
-                WriteToFile(schema_data,fptr);
+                WriteToFile(schema_data,schemaNr,fptr);
                 break;
             }
             else {
@@ -32,7 +51,7 @@ void ResultOut(Exam_block_t *****schema_data) {
     
     fclose(fptr);
 }
-void WriteToFile(Exam_block_t *****schema_data, FILE *fptr) {
+void WriteToFile(Exam_block_t *****schema_data, int schemaNr, FILE *fptr) {
     int i,k,j,m;
     fptr = fopen("best_schema.txt", "w");
     fprintf(fptr,"Best solution.\n");
@@ -52,14 +71,14 @@ void WriteToFile(Exam_block_t *****schema_data, FILE *fptr) {
                 for(i = 0; i < EXAM_SIZE; i++)
                 {
                     fprintf(fptr, "For exam: %i",i);
-                    fprintf(fptr, "Exam name: %s",schema_data[0][k][j][m][i].subjects.subject);
-                    fprintf(fptr, "Class: %c.%i\n",schema_data[0][k][j][m][i].classname,schema_data[0][k][j][m][i].year);
-                    fprintf(fptr, "Student id: %i\n",schema_data[0][k][j][m][i].student.unikID);
-                    fprintf(fptr, "Student name: %s %s\n\n",schema_data[0][k][j][m][i].student.name,schema_data[0][k][j][m][i].student.surname);
-                    fprintf(fptr, "Teacher id: %i\n",schema_data[0][k][j][m][i].subjects.teacher.ID);
-                    fprintf(fptr, "Teacher name: %s %s\n\n",schema_data[0][k][j][m][i].subjects.teacher.name,schema_data[0][k][j][m][i].subjects.teacher.surname);
-                    fprintf(fptr, "Censor id: %i \n",schema_data[0][k][j][m][i].subjects.censor.ID);
-                    fprintf(fptr, "Censor name: %s %s\n",schema_data[0][k][j][m][i].subjects.censor.name,schema_data[0][k][j][m][i].subjects.censor.surname);
+                    fprintf(fptr, "Exam name: %s",schema_data[schemaNr][k][j][m][i].subjects.subject);
+                    fprintf(fptr, "Class: %c.%i\n",schema_data[schemaNr][k][j][m][i].classname,schema_data[0][k][j][m][i].year);
+                    fprintf(fptr, "Student id: %i\n",schema_data[schemaNr][k][j][m][i].student.unikID);
+                    fprintf(fptr, "Student name: %s %s\n\n",schema_data[schemaNr][k][j][m][i].student.name,schema_data[0][k][j][m][i].student.surname);
+                    fprintf(fptr, "Teacher id: %i\n",schema_data[schemaNr][k][j][m][i].subjects.teacher.ID);
+                    fprintf(fptr, "Teacher name: %s %s\n\n",schema_data[schemaNr][k][j][m][i].subjects.teacher.name,schema_data[0][k][j][m][i].subjects.teacher.surname);
+                    fprintf(fptr, "Censor id: %i \n",schema_data[schemaNr][k][j][m][i].subjects.censor.ID);
+                    fprintf(fptr, "Censor name: %s %s\n",schema_data[schemaNr][k][j][m][i].subjects.censor.name,schema_data[0][k][j][m][i].subjects.censor.surname);
 
 
                 }
